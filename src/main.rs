043e8b4c695e37591e93d03c41a13159feccf99f
ibx34 <guamjust@gmail.com>
2025-06-24 22:24:38 -0400

    update 2

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ mod kek;
 mod secure_buf;
 use std::mem;
 
+use crate::secure_buf::SecureBuffer;
+
 fn main() {
-    // Enable OpenSSL fips mode. We want to use FIPS approved modules 
+    // Enable OpenSSL fips mode. We want to use FIPS approved modules
     // only and in specific AES-256-GCM amongst others.
     mem::forget(Provider::load(None, "fips").unwrap());
-    println!("Hello, world!");
+    let secure_buffer: SecureBuffer =
+        SecureBuffer::from_slice(b"Hello, World!".as_slice()).unwrap();
+
+    println!(
+        "{:?}",
+        String::from_utf8(secure_buffer.expose().to_vec()).unwrap()
+    );
+    drop(secure_buffer);
 }
--- src/secure_buf.rs
+++ src/secure_buf.rs
@@ -1,13 +1,53 @@
+use libc::{mlock, munlock};
+
 pub(crate) struct SecureBuffer(*mut u8, usize);
+impl SecureBuffer {
+    pub fn new(size: usize) -> Result<Self, &'static str> {
+        let layout =
+            std::alloc::Layout::from_size_align(size, 4096).map_err(|_| "Invalid layout")?;
+        let ptr = unsafe { std::alloc::alloc_zeroed(layout) };
+        if ptr.is_null() {
+            return Err("Allocation failed");
+        }
+        if unsafe { mlock(ptr as *const libc::c_void, size) } != 0 {
+            unsafe { std::alloc::dealloc(ptr, layout) };
+            return Err("mlock failed");
+        }
+        Ok(Self(ptr, size))
+    }
+
+    pub fn from_slice(data: &[u8]) -> Result<Self, &'static str> {
+        let mut buf = Self::new(data.len())?;
+        unsafe {
+            std::ptr::copy_nonoverlapping(data.as_ptr(), buf.0, data.len());
+        }
+        Ok(buf)
+    }
+
+    pub fn expose(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.0, self.1) }
+    }
+
+    pub fn expose_mut(&mut self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.0, self.1) }
+    }
+}
+
+impl Drop for SecureBuffer {
+    fn drop(&mut self) {
+        let layout = std::alloc::Layout::from_size_align(self.1, 4096).expect("Valid layout");
+
+        unsafe {
+            std::ptr::write_bytes(self.0, 0, self.1);
+            munlock(self.0 as *const libc::c_void, self.1);
+            std::alloc::dealloc(self.0, layout);
+        }
+    }
+}
 unsafe impl Send for SecureBuffer {}
 unsafe impl Sync for SecureBuffer {}
 impl Clone for SecureBuffer {
     fn clone(&self) -> Self {
         panic!("Attempted clone of secure buffer not allowed.")
-    }
-}
-impl Drop for SecureBuffer {
-    fn drop(&mut self) {
-        panic!("Drop")
     }
 }
