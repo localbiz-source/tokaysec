c27899316dce9af95ea1d84a6e068320a65f15b3
ibx34 <guamjust@gmail.com>
2025-07-01 22:42:05 -0400

    update policy related items

--- migrations/20250630024703_initial_tables.sql
+++ migrations/20250630024703_initial_tables.sql
@@ -23,15 +23,22 @@ CREATE TABLE IF NOT EXISTS tokaysec.credentials (
 CREATE TABLE IF NOT EXISTS tokaysec.roles {
     "id" TEXT NOT NULL UNIQUE PRIMARY KEY,
     "name" TEXT NOT NULL UNIQUE,
-    "permissions" TEXT[],
-    "scope_level" TEXT,
+    "scope_level" TEXT, -- What level created it : instance, namespace, project
+    "defined_by" TEXT NOT NULL -- proj_, nmsp_, inst_
 };
 
 CREATE TABLE IF NOT EXISTS tokaysec.policy_target_rule (
     "id" TEXT NOT NULL UNIQUE PRIMARY KEY,
-    "target" TEXT NOT NULL -- person_<id>, role_<id>,
+    "target" TEXT NOT NULL -- person_<id>, role_<id>, perm_<permission>
     "action" INT NOT NULL DEFAULT 0, -- 0 = Deny, 1 = Allow
     "resource" TEXT NOT NULL -- Resource is instance, namespace, project, secret
+);
+
+CREATE TABLE IF NOT EXISTS tokaysec.permissions (
+    "id" TEXT NOT NULL UNIQUE PRIMARY KEY,
+    "target" TEXT NOT NULL, -- role_<id>, person_<id> : ability to assign permissions to both roles and people.
+    "permission" TEXT NOT NULL,
+    "added_when" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
 );
 
 CREATE TABLE IF NOT EXISTS tokaysec.projects {
--- src/models.rs
+++ src/models.rs
@@ -17,3 +17,23 @@ pub struct StoredSecret {
     pub id: String,
     pub secret: serde_json::Value,
 }
+/*
+
+Name scoping : relating a named item to a certain stratum
+
+Namespace, Project - Order matters. Insstance level scope is global, no prefix.
+
+<namespace>-<project>-<name of obj> 
+<project>-<name of obj>
+
+This allows for the same name that makes sense for a position to
+be applied in different namespaces or projects and scoped to the
+relative poeple and permissions. Exmaple:
+
+Namespaces can be thought of as teams that share the same
+instance of TokaySec. So two devteams focusing on different
+tasks but both need access to secrets management. These namespaces
+are isolated and scoped within so projects, and roles, and permissions,
+cannot be shared between them unless specified globally in the instance
+level policy.
+*/
